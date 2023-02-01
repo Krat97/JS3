e61bf0c87e220ec0a1086962d56629170bae3c15
Krat97 <ol.maksymiuk@gmail.com>
2023-02-01 19:27:46 +0200

    Merge remote-tracking branch 'github/master'

---
+++ JS/JS2/js.js
@@ -0,0 +1,137 @@
+// modules = ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java'],
+//     console.log(modules)
+
+
+
+// let bookc = {  title : 'Люба назва книги',
+//                 pageCount : 200,
+//                 genre : 'Любий жанр',
+//                 authors : { name : 'A', age : 'B'}
+//             }
+// console.log(bookc);
+
+
+
+
+// let baza = [
+//     user = {
+//         name: 'A',
+//         username:'B',
+//         password:123
+//     },user2 = {
+//         name: 'A',
+//         username:'B',
+//         password:123
+//     }, user3 = {
+//         name: 'A',
+//         username:'B',
+//         password:123
+//     }, user4 = {
+//         name: 'A',
+//         username:'B',
+//         password:123
+//     }, user5 = {
+//         name: 'A',
+//         username:'B',
+//         password:123
+//     }, user6 = {
+//         name: 'A',
+//         username:'B',
+//         password:123
+//     }, user7 = {
+//         name: 'A',
+//         username:'B',
+//         password:123
+//     }, user8 = {
+//         name: 'A',
+//         username:'B',
+//         password:123
+//             }];
+// console.log(baza);
+
+
+
+// let a = 'svs';
+// if (a > 0 || a < 0 ){
+//     console.log('Вірно');
+// }
+// else if  (a ===0){
+//     console.log('Не вірно');
+// }
+// else {
+//     console.log('???')};
+
+
+
+
+// let tatle = 16;
+// if ( tatle>0 && tatle<16){
+//     console.log(1)}
+// else if ( tatle>15 && tatle<31){
+//     console.log(2)}
+// else if ( tatle>30 && tatle<46){
+//     console.log(3)}
+// else if ( tatle>45 && tatle<61){
+//     console.log(4)}
+// else {
+//     console.log('???')}
+
+
+
+// let dey = 2;
+// switch (dey){
+//     case 1:
+//         console.log('1');
+//         break;
+//     case 2:
+//         console.log('2');
+//         break;
+//     case 3:
+//         console.log('3');
+//         break;
+//     case 4:
+//         console.log('4');
+//         break;
+//     case 5:
+//         console.log('5');
+//         break;
+//
+//     default :
+//         console.log('???')
+// }
+
+
+
+// let a = 5 ;
+// let b = 8 ;
+// if (a>b){
+//     console.log(a);}
+// else if (b>a){
+//     console.log(b);}
+// else if (a===b){
+//     console.log('рівні');}
+// else {
+//     console.log('MMM');
+// }
+
+
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+//     if (coursesAndDurationArray[0].monthDuration > 5) {
+//         console.log( monthDuration + 'CLAS')}
+//     if (coursesAndDurationArray[1].monthDuration > 5) {
+//         console.log( coursesAndDurationArray[1].monthDuration + ' CLAS')}
+//     if (coursesAndDurationArray[2].monthDuration > 5) {
+//         console.log( coursesAndDurationArray[2].monthDuration + ' CLAS')}
+//     if (coursesAndDurationArray[3].monthDuration > 5) {
+//         console.log( coursesAndDurationArray[3].monthDuration + ' CLAS')}
+//     if (coursesAndDurationArray[4].monthDuration > 5) {
+//         console.log( coursesAndDurationArray[4].monthDuration + ' CLAS')}
+//     if (coursesAndDurationArray[5].monthDuration > 5) {
+//         console.log( coursesAndDurationArray[5].monthDuration + ' CLAS')}
