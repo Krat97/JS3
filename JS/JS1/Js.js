a2681044bb0d08e84949fa70cf2162ec1ca16bf9
Krat97 <ol.maksymiuk@gmail.com>
2023-02-01 19:11:09 +0200

    Merge remote-tracking branch 'github/master'

---
+++ JS/JS1/Js.js
@@ -0,0 +1,43 @@
+// let q = 'hello';
+// let w = 'owu';
+// let e = 'com';
+// let r = 'ua';
+// let t = 1;
+// let y = 10;
+// let u = -999;
+// let i = 123;
+// let o = 3.14;
+// let p = 2.7;
+// let a = 16;
+// let s = true;
+// let d = false;
+//
+// console.log(q)
+// console.log(w)
+// console.log(e)
+// console.log(r)
+// console.log(t)
+// console.log(y)
+// console.log(u)
+// console.log(i)
+// console.log(o)
+// console.log(p)
+// console.log(a)
+// console.log(s)
+// console.log(d)
+
+let firstName = 'Maksymiuk';
+let middleName = 'Oleh';
+let lastName = 'Ivanovich';
+let person = (firstName +middleName + lastName);
+console.log(person)
+
+let a =100;
+let b = '100';
+let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+
+
