a72d33cffc1a2bd7ade5aa396ca1e2e0ddc04ad2
Krat97 <ol.maksymiuk@gmail.com>
2023-02-09 19:20:12 +0200

    Merge remote-tracking branch 'origin/master'

--- JS/JS3/JS.js
+++ JS/JS3/JS.js
@@ -1,2 +1,8 @@
-for (let i =0; i<10; i++)
-    document.write( i,`<div>пентагон</div>`);
+// for (let i =0; i<10; i++)
+//     document.write( i,`<div>пентагон</div>`);
+
+let i = 0;
+while (i < 20); {
+    i++;}
+    console.log( i +' Пентагон');
+
